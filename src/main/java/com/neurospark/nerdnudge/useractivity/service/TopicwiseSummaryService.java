e1d957f172a23930ad354288d7a1e6fea2b149e4
Rajeev Dalavi <rajeev.dalavi@gmail.com>
2024-08-08 11:22:23 +0530

    Topicwise last 30 days updated for user profiles.

--- src/main/java/com/neurospark/nerdnudge/useractivity/service/TopicwiseSummaryService.java
+++ src/main/java/com/neurospark/nerdnudge/useractivity/service/TopicwiseSummaryService.java
@@ -10,16 +10,17 @@ import java.util.Map;
 
 public class TopicwiseSummaryService {
     public void updateTopicwiseSummary(JsonObject userData, UserActivityCounts counts) {
-        updateTopicwiseOverallSummary(userData, counts);
-        updateTopicwiseLast30DaysSummary(userData, counts);
+        JsonElement topicwiseEle = userData.get("topicwise");
+        JsonObject topicwiseObject = (topicwiseEle == null || topicwiseEle.isJsonNull()) ? new JsonObject() : topicwiseEle.getAsJsonObject();
+        userData.add("topicwise", topicwiseObject);
+
+        updateTopicwiseOverallSummary(topicwiseObject, counts);
+        updateTopicwiseLast30DaysSummary(topicwiseObject, counts);
     }
 
-    private void updateTopicwiseOverallSummary(JsonObject userData, UserActivityCounts counts) {
+    private void updateTopicwiseOverallSummary(JsonObject topicwiseObject, UserActivityCounts counts) {
         try {
             System.out.println("Updating topicwise summary now.");
-            JsonElement topicwiseEle = userData.get("topicwise");
-            JsonObject topicwiseObject = (topicwiseEle == null || topicwiseEle.isJsonNull()) ? new JsonObject() : topicwiseEle.getAsJsonObject();
-
             JsonElement topicwiseOverallEle = topicwiseObject.get("overall");
             JsonObject topicwiseOverallObject = (topicwiseOverallEle == null || topicwiseOverallEle.isJsonNull()) ? new JsonObject() : topicwiseOverallEle.getAsJsonObject();
             topicwiseObject.add("overall", topicwiseOverallObject);
@@ -58,14 +59,67 @@ public class TopicwiseSummaryService {
                     subtopicsObject.add(subtopic, thisSubtopicUpdatedArray);
                 }
             }
-            userData.add("topicwise", topicwiseObject);
         }
         catch(Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    private void updateTopicwiseLast30DaysSummary(JsonObject userData, UserActivityCounts counts) {
+    private void updateTopicwiseLast30DaysSummary(JsonObject topicwiseObject, UserActivityCounts counts) {
+        System.out.println("Updating topicwise last 30 days now.");
+        JsonElement topicwiseLast30DaysEle = topicwiseObject.get("last30Days");
+        JsonObject topicwiseLast30DaysObject = (topicwiseLast30DaysEle == null || topicwiseLast30DaysEle.isJsonNull()) ? new JsonObject() : topicwiseLast30DaysEle.getAsJsonObject();
+        topicwiseObject.add("last30Days", topicwiseLast30DaysObject);
 
+        Map<String, int[]> topicSummaryCounts = counts.getTopicsSummaryCounts();
+        for (String topic : topicSummaryCounts.keySet()) {
+            JsonElement thisTopicDetailsFromUserDataEle = topicwiseLast30DaysObject.get(topic);
+            JsonObject thisTopicDetailsFromUserDataObject = (thisTopicDetailsFromUserDataEle == null || thisTopicDetailsFromUserDataEle.isJsonNull()) ? new JsonObject() : thisTopicDetailsFromUserDataEle.getAsJsonObject();
+            topicwiseLast30DaysObject.add(topic, thisTopicDetailsFromUserDataObject);
+
+            updateLast30DaysSummary(thisTopicDetailsFromUserDataObject, topic, topicSummaryCounts);
+            updateLast30DaysSubtopics(thisTopicDetailsFromUserDataObject, topic, counts.getSubtopicsCounts().get(topic));
+        }
+    }
+
+    private void updateLast30DaysSubtopics(JsonObject thisTopicDetailsFromUserDataObject, String topic, Map<String, int[]> subtopicCounts) {
+        JsonElement subtopicsEle = thisTopicDetailsFromUserDataObject.get("subtopics");
+        JsonObject subtopicsObject = (subtopicsEle == null || subtopicsEle.isJsonNull()) ? new JsonObject() : subtopicsEle.getAsJsonObject();
+        thisTopicDetailsFromUserDataObject.add("subtopics", subtopicsObject);
+
+        String currentDay = Commons.getInstance().getDaystamp();
+        for(String subtopic: subtopicCounts.keySet()) {
+            JsonElement subtopicEle = subtopicsObject.get(subtopic);
+            JsonObject subtopicObject = (subtopicEle == null || subtopicEle.isJsonNull()) ? new JsonObject() : subtopicEle.getAsJsonObject();
+            subtopicsObject.add(subtopic, subtopicObject);
+
+            JsonArray updatedArray = Commons.getInstance().getUpdatedArray(subtopicObject.get(currentDay), subtopicCounts.get(subtopic)[0], subtopicCounts.get(subtopic)[1]);
+            subtopicObject.add(currentDay, updatedArray);
+
+            Commons.getInstance().housekeepDayJsonObject(subtopicObject, 30);
+        }
+    }
+
+    private void updateLast30DaysSummary(JsonObject thisTopicDetailsFromUserDataObject, String topic, Map<String, int[]> topicSummaryCounts) {
+        JsonElement summaryEle = thisTopicDetailsFromUserDataObject.get("summary");
+        JsonObject summaryObject = (summaryEle == null || summaryEle.isJsonNull()) ? new JsonObject() : summaryEle.getAsJsonObject();
+        thisTopicDetailsFromUserDataObject.add("summary", summaryObject);
+
+        String currentDay = Commons.getInstance().getDaystamp();
+        JsonElement currentDayEle = summaryObject.get(currentDay);
+        JsonArray currentDayArray = (currentDayEle == null || currentDayEle.isJsonNull()) ? new JsonArray() : currentDayEle.getAsJsonArray();
+        summaryObject.add(currentDay, currentDayArray);
+
+        int[] currentCounts = topicSummaryCounts.get(topic);
+        if (currentDayArray.size() > 0) {
+            currentDayArray.set(0, new JsonPrimitive(currentDayArray.get(0).getAsInt() + currentCounts[0]));
+            currentDayArray.set(1, new JsonPrimitive(currentDayArray.get(1).getAsInt() + currentCounts[1]));
+            currentDayArray.set(2, new JsonPrimitive(currentDayArray.get(2).getAsInt() + currentCounts[2]));
+        } else {
+            currentDayArray.add(new JsonPrimitive(currentCounts[0]));
+            currentDayArray.add(new JsonPrimitive(currentCounts[1]));
+            currentDayArray.add(new JsonPrimitive(currentCounts[2]));
+        }
+        Commons.getInstance().housekeepDayJsonObject(summaryObject, 30);
     }
 }
